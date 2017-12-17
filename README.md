d4d6985b3fd9b14f4b95cd14e1f8db605890aea3
pepc <pcondal@apsic.com>
2017-12-17 01:57:41 +0100

    Added support for multiple sites (hashed site names)

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # One Tab per Job for Matecat
-This Chrome extension ensures that the existing Chrome tab of a [Matecat](https://www.matecat.com) job is reused when chosing **Edit Source (Ctrl+E)** from [ApSIC Xbench](http://www.xbench.net) to edit a translation segment of such Matecat job.
+This Chrome extension ensures that the existing Chrome tab of a [Matecat](https://www.matecat.com) job is reused when chosing **Edit Segment (Ctrl+E)** from [ApSIC Xbench](http://www.xbench.net) to edit a translation segment of such Matecat job.
 
-It avoids the clutter of tab proliferation as you repeteadly choose **Edit Source** from Xbench and, most importantly, it also avoids running out of sockets against the Matecat cloud server when the user has opened too many Matecat tabs, which appears to be a hang (the user gets a blank page until she closes another Matecat tab).
+It avoids the clutter of tab proliferation as you repeteadly choose **Edit Segment** from Xbench and, most importantly, it also avoids running out of sockets against the Matecat cloud server when the user has opened too many Matecat tabs, which appears to be a hang (the user gets a blank page until she closes another Matecat tab).
+
+**NOTE:** This Chrome extension only works for known Matecat sites.  If you run a local Matecat site and wish to be recognized by this Chrome Extension as to improve the user experience, please [contact us](https://www.xbench.net/index.php/contact-us) to add your Matecat site to the list of recognized sites.  There is no cost involved in adding a new site to the list.
 
 This Chrome Extension is published on the [Chrome Web Store](https://chrome.google.com/webstore/detail/one-tab-per-job-for-matec/amlgbghdoipfolgicbijclknldenpijf) and requires the "_Read your browsing history_" permision because it has to locate the Chrome tab for the Matecat job among all tabs that are currently open in Chrome.
 
--- background.js
+++ background.js
@@ -1,22 +1,58 @@
+function sha256(str) {
+    // We transform the string into an arraybuffer.
+    var buffer = new TextEncoder("utf-8").encode(str);
+    return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
+        return hex(hash);
+    });
+}
+
+function hex(buffer) {
+    var hexCodes = [];
+    var view = new DataView(buffer);
+    for (var i = 0; i < view.byteLength; i += 4) {
+        // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+        var value = view.getUint32(i)
+        // toString(16) will give the hex representation of the number without padding
+        var stringValue = value.toString(16)
+        // We use concatenation and slice for padding
+        var padding = '00000000'
+        var paddedValue = (padding + stringValue).slice(-padding.length)
+        hexCodes.push(paddedValue);
+    }
+
+    // Join all the hex strings into one
+    return hexCodes.join("");
+}
+
+var getHostName = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l.hostname;
+}
+
+var sites = [
+    "5817e30b60c7c20dc16c037da9f32244ca040610b1420fc49c47a268d0f4e76e", // wmc
+    "bd9e3643adb1e3f3e0df67485832e9b8e2dbe85dac9b926e695bc7e60b2347bc", // bmc
+    "f69dff9562a3d1cf7e01c5526669f431f8a0d966c86556a79f823b718fee5929"  // cpn
+]
+
 chrome.tabs.onCreated.addListener(function(newTab) {
     chrome.tabs.getAllInWindow(newTab.windowId, function(tabs) {
-    	var matecatTranslate = "https://www.matecat.com/translate/";
-    	var matecatRevise = "https://www.matecat.com/revise/";
-
-      // Check if we have the Matecat job open to avoid opening a second one
-      // and running out of sockets
-    	if ((newTab.url.lastIndexOf(matecatTranslate, 0) === 0) ||
-    		  (newTab.url.lastIndexOf(matecatRevise, 0) === 0)) {
-        var duplicateTab = null;
-        tabs.forEach(function(otherTab) {
-            if (otherTab.id !== newTab.id && otherTab.url.split('#')[0] === newTab.url.split('#')[0]) {
-                duplicateTab = otherTab;
+        sha256(getHostName(newTab.url)).then(function(digest) {
+            if (sites.indexOf(digest) >= 0) {
+                // Check if we have the Matecat job open to avoid opening a second one
+                // and running out of sockets
+                var duplicateTab = null;
+                tabs.forEach(function(otherTab) {
+                    if (otherTab.id !== newTab.id && otherTab.url.split('#')[0] === newTab.url.split('#')[0]) {
+                        duplicateTab = otherTab;
+                    }
+                });
+                if (duplicateTab) {
+                    chrome.tabs.remove(newTab.id);
+                    chrome.tabs.update(duplicateTab.id, {"selected": true, "url": newTab.url});
+                }
             }
         });
-        if (duplicateTab) {
-            chrome.tabs.remove(newTab.id);
-            chrome.tabs.update(duplicateTab.id, {"selected": true, "url": newTab.url});
-        }
-      }
     });
 });
