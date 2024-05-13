a0f33f9c0caf9ca77657a81758b2279f44560382
pepc <pcondal@apsic.com>
2024-05-13 18:53:36 +0200

    Smartcat fixes for v2

--- background.js
+++ background.js
@@ -1,21 +1,32 @@
 const MATECAT_REGEX = "/(translate|revise)/[^/]+/[a-zA-Z-]+/[0-9]+-[0-9a-z]+#[0-9]+$";
-const SMARTCAT_REGEX = "smartcat\.ai/editor\?.*DocumentId=[0-9a-f]+.*LanguageId=[0-9]+";
+const SMARTCAT_REGEX = "smartcat\.(ai|com)\/editor\?.*[Dd]ocumentId=[0-9a-f]+.*[Ll]anguageId=[0-9]+";
 const LINGOTEK_REGEX = "/workbench/task/[0-9a-f\-]+/segment/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
 const CROWDIN_REGEX = "crowdin\.com/translate/.*#";
 
 const MATECAT_SEGMENT_LABEL = "#";
-const SMARTCAT_SEGMENT_LABEL = "&SegmentIndex=";
+const SMARTCAT_SEGMENT_LABEL = "&segment";
 const LINGOTEK_SEGMENT_LABEL = "/task/";
 const CROWDIN_SEGMENT_LABEL = "#";
 
-function HasPatternThenProcess(newTab, tabs, pattern, segmentLabel, killOriginalTab) {
+function IsSameDocument(existingTabPrefix, newTabPrefix, segmentLabel, caseSensitive) {
+    if (caseSensitive) {
+		var leftText = existingTabPrefix;
+		var rightText = newTabPrefix;
+		var label = segmentLabel;
+    } else {
+		var leftText = existingTabPrefix.toLowerCase();
+		var rightText = newTabPrefix.toLowerCase();
+		var label = segmentLabel.toLowerCase();
+	}
+	return leftText.split(label)[0] === rightText.split(label)[0];
+}
+
+function HasPatternThenProcess(newTab, tabs, pattern, segmentLabel, killOriginalTab, caseSensitive) {
     var regex = new RegExp(pattern);
     if (regex.test(newTab.url)) {
-        // Check if we have the job open to avoid opening a second one
-        // and running out of sockets
         var duplicateTab = null;
         tabs.forEach(function(otherTab) {
-            if (otherTab.id !== newTab.id && otherTab.url.split(segmentLabel)[0] === newTab.url.split(segmentLabel)[0]) {
+            if (otherTab.id !== newTab.id && IsSameDocument(otherTab.url, newTab.url, segmentLabel, caseSensitive)) {
                 duplicateTab = otherTab;
             }
         });
@@ -36,10 +47,10 @@ lastId = -1;
 
 function reuseExistingEditor(tab) {
     chrome.tabs.getAllInWindow(tab.windowId, function(tabs) {
-        if (HasPatternThenProcess(tab, tabs, MATECAT_REGEX, MATECAT_SEGMENT_LABEL, false)) return;
-        if (HasPatternThenProcess(tab, tabs, SMARTCAT_REGEX, SMARTCAT_SEGMENT_LABEL, false)) return;
-        if (HasPatternThenProcess(tab, tabs, LINGOTEK_REGEX, LINGOTEK_SEGMENT_LABEL, false)) return;
-        if (HasPatternThenProcess(tab, tabs, CROWDIN_REGEX, CROWDIN_SEGMENT_LABEL, true)) return;
+        if (HasPatternThenProcess(tab, tabs, MATECAT_REGEX, MATECAT_SEGMENT_LABEL, false, true)) return;
+        if (HasPatternThenProcess(tab, tabs, SMARTCAT_REGEX, SMARTCAT_SEGMENT_LABEL, false, false)) return;
+        if (HasPatternThenProcess(tab, tabs, LINGOTEK_REGEX, LINGOTEK_SEGMENT_LABEL, false, true)) return;
+        if (HasPatternThenProcess(tab, tabs, CROWDIN_REGEX, CROWDIN_SEGMENT_LABEL, true, true)) return;
     });
     lastId = -1;
 }
@@ -52,7 +63,7 @@ chrome.tabs.onCreated.addListener(function(newTab) {
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
-    if ((tabId == lastId) && (info.url)) {
+	if ((tabId == lastId) && (info.url)) {
         reuseExistingEditor(tab);
     }
 });
